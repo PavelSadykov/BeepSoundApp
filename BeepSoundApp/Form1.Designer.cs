69850768138e8bfb3af44e9a772ee1a83c1332c1
PavelSadykov <2439747@gmail.com>
2023-12-12 16:55:03 +0500

    Добавьте файлы проекта.

---
+++ BeepSoundApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BeepSoundApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessageBeep = new System.Windows.Forms.Button();
+            this.btnBeep = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMessageBeep
+            // 
+            this.btnMessageBeep.Location = new System.Drawing.Point(492, 214);
+            this.btnMessageBeep.Name = "btnMessageBeep";
+            this.btnMessageBeep.Size = new System.Drawing.Size(75, 23);
+            this.btnMessageBeep.TabIndex = 3;
+            this.btnMessageBeep.Text = "Сигнал2";
+            this.btnMessageBeep.UseVisualStyleBackColor = true;
+            this.btnMessageBeep.Click += new System.EventHandler(this.btnMessageBeep_Click);
+            // 
+            // btnBeep
+            // 
+            this.btnBeep.Location = new System.Drawing.Point(234, 214);
+            this.btnBeep.Name = "btnBeep";
+            this.btnBeep.Size = new System.Drawing.Size(75, 23);
+            this.btnBeep.TabIndex = 2;
+            this.btnBeep.Text = "Сигнал1";
+            this.btnBeep.UseVisualStyleBackColor = true;
+            this.btnBeep.Click += new System.EventHandler(this.btnBeep_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMessageBeep);
+            this.Controls.Add(this.btnBeep);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessageBeep;
+        private System.Windows.Forms.Button btnBeep;
+    }
+}
+
---
+++ BeepSoundApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace BeepSoundApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int frequency, int duration);
+
+        [DllImport("user32.dll")]
+      
+        public static extern bool MessageBeep(uint uType);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBeep_Click(object sender, EventArgs e)
+        {
+            int frequency = 450; // Частота звука (в герцах)
+            int duration = 1000;   // Продолжительность звука (в миллисекундах)
+
+            for (int i = 0; i < 10; i++)
+            {
+                Beep(frequency, duration);
+                Thread.Sleep(1000); // Пауза между звуками
+            }
+        }
+
+        private void btnMessageBeep_Click(object sender, EventArgs e)
+        {
+
+           
+                MessageBeep((uint)MessageBoxIcon.Asterisk);   // Звук "Asterisk"
+                Thread.Sleep(1000); // Пауза 
+           
+        }
+    }
+
+    }
